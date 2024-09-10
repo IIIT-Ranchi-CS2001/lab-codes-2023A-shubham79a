25abaf18c042c9cca3a6a09c01c51e95c189c6c0
shubham79a <146449583+shubham79a@users.noreply.github.com>
2024-09-10 12:24:32 +0530

    Add files via upload

---
+++ Class 04 September 10th 2k24/problem_05.py
@@ -0,0 +1,6 @@
+s=input()
+
+if s.isalnum():
+    print("True")
+else :
+    print("False")
---
+++ Class 04 September 10th 2k24/problem_06.py
@@ -0,0 +1,9 @@
+string=input()
+
+# checking a frequency
+count=0
+for char in string:
+    if char=="a":
+        count+=1
+
+print(count)
